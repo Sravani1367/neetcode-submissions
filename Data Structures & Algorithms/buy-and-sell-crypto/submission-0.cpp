b2857640c28481e230bdaf87cf8bef9339de4c68
Sravani1367 <sravanipentakota2025@gmail.com>
2026-08-25 20:35:30 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=INT_MAX;
+        int maxi=0;
+        for(int price:prices){
+            mini=min(mini,price);
+            maxi=max(maxi,price-mini);
+        }
+        return maxi;
+    }
+
+};
